6f1df5ee616c5a59cb0bc421de962b0bf896c1e5
siamgm <siamgmteam@gmail.com>
2025-01-09 12:58:50 +0600

    cvdv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
 //  Change header styles on scroll
- const header = document.querySelector('.header');
- window.addEventListener('scroll', () => {
-   if (window.scrollY > 60) {
-     header.classList.add('bg-white', 'text-black');
-     header.classList.remove('bg-transparent', 'text-white');
-   } else {
-     header.classList.add('bg-transparent', 'text-white');
-     header.classList.remove('bg-white', 'text-black');
-   }
- });
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 60) {
+    header.classList.add('bg-white', 'text-black');
+    header.classList.remove('bg-transparent', 'text-white');
+  } else {
+    header.classList.add('bg-transparent', 'text-white');
+    header.classList.remove('bg-white', 'text-black');
+  }
+});
 
- // Toggle menu for small screens
+// Toggle menu for small screens
 //  document.getElementById('menu-toggle').addEventListener('click', () => {
 //     menu.classList.toggle('hidden');
 //   });
 
 
 //   home slider
-
