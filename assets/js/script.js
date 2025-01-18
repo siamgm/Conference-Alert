3c026c98477f335d6aba87d3f25808b6dd0dac8f
siamgm <siamgmteam@gmail.com>
2025-01-18 09:47:14 +0600

    16/01/2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,26 @@
 const header = document.querySelector('.header');
 const headerSearchField = document.querySelector('.search-field');
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 60) {
-    header.classList.add('bg-white', 'text-black');
-    header.classList.remove('bg-transparent', 'text-white');
-    headerSearchField.classList.remove('bg-transparent');
-  } else {
-    header.classList.add('bg-transparent', 'text-white');
-    header.classList.remove('bg-white', 'text-black');
-    headerSearchField.classList.add('bg-transparent');
+  if (window.innerWidth > 1024) { // Apply only for screens wider than 1024px
+    if (window.scrollY > 60) {
+      header.classList.add('bg-white', 'text-black');
+      header.classList.remove('bg-transparent', 'text-white');
+      headerSearchField.classList.remove('bg-transparent');
+    } else {
+      header.classList.add('bg-transparent', 'text-white');
+      header.classList.remove('bg-white', 'text-black');
+      headerSearchField.classList.add('bg-transparent');
+    }
   }
 });
 
+
 // Toggle menu for small screens
-//  document.getElementById('menu-toggle').addEventListener('click', () => {
-//     menu.classList.toggle('hidden');
-//   });
+
+const menu = document.getElementById('menu');
+ document.getElementById('menu-toggle').addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
 
 
 //   tabs
@@ -58,17 +63,38 @@ if (tabButtons.length > 0) {
 function switchTab(event, tabId) {
   // Remove active state from all tabs
   document.querySelectorAll('.tab-header').forEach(tab => {
-      tab.classList.remove('bg-red-500', 'text-white');
-      tab.classList.add('bg-gray-100', 'text-gray-700');
+    tab.classList.remove(
+      'bg-red-500',
+      'text-white',
+      'md:before:bg-red-500',
+      'before:h-3',
+      'before:w-3',
+      'before:absolute',
+      'before:right-[-6px]',
+      'before:bottom-[33%]',
+      'before:rotate-45'
+    );
+    tab.classList.add('bg-gray-100', 'text-gray-700');
   });
 
   // Add active state to the clicked tab
-  event.target.classList.add('bg-red-500', 'text-white');
-  event.target.classList.remove('bg-gray-100', 'text-gray-700');
+  const clickedTab = event.currentTarget;
+  clickedTab.classList.add(
+    'bg-red-500',
+    'text-white',
+    'md:before:bg-red-500',
+    'before:h-3',
+    'before:w-3',
+    'before:absolute',
+    'before:right-[-6px]',
+    'before:bottom-[33%]',
+    'before:rotate-45'
+  );
+  clickedTab.classList.remove('bg-gray-100', 'text-gray-700');
 
   // Hide all tab contents
   document.querySelectorAll('.tab-content').forEach(content => {
-      content.classList.add('hidden');
+    content.classList.add('hidden');
   });
 
   // Show the selected tab content
