787d51b93e7862256a6c118918084bf889ac4ecf
siamgm <siamgmteam@gmail.com>
2025-01-23 09:54:45 +0600

    kuy

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,26 @@ menu.addEventListener('click', (event) => {
 
 // tab
 
-function switchTab(event, tabId) {
-  // Remove active state from all tabs
-  document.querySelectorAll('.tab-header').forEach(tab => {
-    tab.classList.remove(
+  function switchTab(event, tabId) {
+    // Remove active state from all tabs
+    document.querySelectorAll('.tab-header').forEach(tab => {
+      tab.classList.remove(
+        'bg-red-500',
+        'text-white',
+        'md:before:bg-red-500',
+        'before:h-3',
+        'before:w-3',
+        'before:absolute',
+        'before:right-[-6px]',
+        'before:bottom-[33%]',
+        'before:rotate-45'
+      );
+      tab.classList.add('bg-gray-100', 'text-gray-700');
+    });
+  
+    // Add active state to the clicked tab
+    const clickedTab = event.currentTarget;
+    clickedTab.classList.add(
       'bg-red-500',
       'text-white',
       'md:before:bg-red-500',
@@ -51,32 +67,19 @@ function switchTab(event, tabId) {
       'before:bottom-[33%]',
       'before:rotate-45'
     );
-    tab.classList.add('bg-gray-100', 'text-gray-700');
-  });
+    clickedTab.classList.remove('bg-gray-100', 'text-gray-700');
+  
+    // Hide all tab contents
+    document.querySelectorAll('.tab-content').forEach(content => {
+      content.classList.add('hidden');
+    });
+  
+    // Show the selected tab content
+    document.getElementById(tabId).classList.remove('hidden');
+  }
 
-  // Add active state to the clicked tab
-  const clickedTab = event.currentTarget;
-  clickedTab.classList.add(
-    'bg-red-500',
-    'text-white',
-    'md:before:bg-red-500',
-    'before:h-3',
-    'before:w-3',
-    'before:absolute',
-    'before:right-[-6px]',
-    'before:bottom-[33%]',
-    'before:rotate-45'
-  );
-  clickedTab.classList.remove('bg-gray-100', 'text-gray-700');
 
-  // Hide all tab contents
-  document.querySelectorAll('.tab-content').forEach(content => {
-    content.classList.add('hidden');
-  });
 
-  // Show the selected tab content
-  document.getElementById(tabId).classList.remove('hidden');
-}
 
 
 // single page calendar btn
